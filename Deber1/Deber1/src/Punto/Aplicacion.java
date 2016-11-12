d1a75bf0f1765138c92bbcad81c14c2faab4affa
daviidyaanez <luiis.daviid4@gmail.com>
2016-11-12 12:22:22 -0500

    Update Aplicacion.java

--- Deber1/Deber1/src/Punto/Aplicacion.java
+++ Deber1/Deber1/src/Punto/Aplicacion.java
@@ -8,8 +8,7 @@ public static void main(String args[]){
 		Circulo cir = new Circulo();
 		Linea lin = new Linea();
 		Cuadrado cuad = new Cuadrado();
-	//	Triangulo tri= new Triangulo();
-	//	Rectangulo rec= new Rectangulo();
+	
 		
 		p.setX(3);
 		p.setY(2);
@@ -22,22 +21,12 @@ public static void main(String args[]){
 		cuad.setPto3(new Punto(8,7));
 		cuad.setPto4(new Punto(8,1));
 		
-		/*tri.setPto1(new Punto(6,1));
-		tri.setPto2(new Punto(10,1));
-		tri.setPto3(new Punto(8,5));
-		
-		rec.setPto1(new Punto(1,0));
-		rec.setPto2(new Punto(4,0));
-		rec.setPto3(new Punto(1,3));
-		rec.setPto4(new Punto(4,3));*/
-		
 		
 		System.out.println("Punto: "+p);
 		System.out.println("Circulo: "+cir);
 		System.out.println("Linea: "+lin);
 		System.out.println("Cuadrado: "+cuad);
-	//	System.out.println("Triangulo: "+tri);
-	//	System.out.println("Rectangulo: "+rec);
+	
 		
 	}
 	
